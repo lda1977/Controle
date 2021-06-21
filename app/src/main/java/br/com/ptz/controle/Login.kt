986528fb7fe91fda960ab11c81726985aeb921b8
Luciano_Amarante <lucianoa2028@gmail.com>
2021-06-21 13:12:27 -0300

    Nova Tela Login.kt 1 - Inserido editText e-mail, senha 2 - Novo button btnPreco 3 - Consulta ao Banco dados 4 - tem erro ao passar a variavel para SistemaMainActivity.kt

--- app/src/main/java/br/com/ptz/controle/Login.kt
+++ app/src/main/java/br/com/ptz/controle/Login.kt
@@ -3,28 +3,83 @@ import android.content.Intent
 import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.AlarmClock.EXTRA_MESSAGE
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-
+import android.widget.Toast
+import br.com.ptz.controle.sistema.SistemaMainActivity
 import java.lang.Exception
 import java.sql.DriverManager
-const val EXTRA_MESSAGE_EMAIL = "br.com.ptz.controle.sistema.MESSAGEEMAIL"
-const val EXTRA_MESSAGE_SENHA = "br.com.ptz.controle.sistema.MESSAGESENHA"
-const val EXTRA_MESSAGE_NOME = "br.com.ptz.controle.sistema.MESSAGENOME"
+
+const val EXTRA_MESSAGE_EMAIL = "br.com.ptz.controle.MESSAGEEMAIL"
+const val EXTRA_MESSAGE_SENHA = "br.com.ptz.controle.MESSAGESENHA"
+const val EXTRA_MESSAGE_NOME = "br.com.ptz.controle.MESSAGENOME"
+
 class Login : AppCompatActivity() {
+
     var text: TextView? = null
     var errorText: TextView? = null
     var show: Button? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        // Mostrando os textView e Button
         text = findViewById<View>(R.id.textView) as TextView
         errorText = findViewById<View>(R.id.textView2) as TextView
         show = findViewById<Button>(R.id.button) as Button
-        show!!.setOnClickListener { Task().execute() }
 
+        // Criando Button
+        val EntranoSistema = findViewById<Button>(R.id.btnLogar) as Button
+
+        // Chamando onclicklistenner
+        EntranoSistema.setOnClickListener {
+
+            // Pegando o conteudo dos campos txtInpEmail e txtInpPassword
+            val tLogin = findViewById<View>(R.id.txtInpEmail) as TextView  // Que é o testo Login
+            val tSenha = findViewById<View>(R.id.txtInpPassword) as TextView  // Que é o testo Senha
+
+            // Salvando em variaveis String Separadas
+            val login = tLogin.text.toString()
+            val senha = tSenha.text.toString()
+
+            // Aqui vou chamara o DBA
+
+            // Chamando a função setOnClickListener
+            show!!.setOnClickListener { Task().execute() }
+
+            // Variáveis de Resultado do DBA
+            val ResultadoLoginDBA = login
+            val ResultadoSenhaDBA = senha
+
+            // Fazendo uma comparação de variáveis
+            if(ResultadoLoginDBA == tLogin.text.toString() && ResultadoSenhaDBA == tSenha.text.toString()){
+                ALERTA("Login com Sucesso!")
+
+                val message = tLogin.text.toString()
+                val messageP = tSenha.text.toString()
+
+                val intent = Intent(this, SistemaMainActivity::class.java).apply {
+                    putExtra(EXTRA_MESSAGE, message)
+                    putExtra(EXTRA_MESSAGE, messageP)
+                }
+                startActivity(intent)
+
+            }else{
+                ALERTA("Erro!!! Login ou Senha incorretos, tente novamente!!!")
+            }
+        }
+        show!!.setOnClickListener { Task().execute() }
     }
+
+    // Criando a função de ALERTA
+    private fun ALERTA ( ResultadoLogin: String){
+        // Aqui vou verificar se foi logado com sucesso ou não
+        Toast.makeText(this, ResultadoLogin, Toast.LENGTH_LONG).show()
+    }
+
     internal inner class Task : AsyncTask<Void?, Void?, Void?>() {
         var records = ""
         var error = ""
--- app/src/main/java/br/com/ptz/controle/sistema/SistemaMainActivity.kt
+++ app/src/main/java/br/com/ptz/controle/sistema/SistemaMainActivity.kt
@@ -1,12 +1,35 @@
 package br.com.ptz.controle.sistema
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import br.com.ptz.controle.R
+
+const val EXTRA_MESSAGE = "br.com.ptz.controle.sistema.MESSAGE"
 
 class SistemaMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sistema_main)
+
+
+        // Criando Button
+        val BTNPreco = findViewById<Button>(R.id.btnPreco) as Button
+
+        // Chamando onclicklistenner
+        BTNPreco.setOnClickListener {
+            val Sistemaintent = Intent(this, SistemaMainActivity::class.java).apply {
+            }
+            startActivity(Sistemaintent)
+
+        }
+        // Get the Intent that started this activity and extract the string
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        findViewById<TextView>(R.id.txtResulEmail).apply {
+            text = message
+        }
     }
 }
