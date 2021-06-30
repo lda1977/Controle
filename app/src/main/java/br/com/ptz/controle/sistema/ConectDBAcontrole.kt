ea9d45d55f681cbc2b12cf0e4b384f60b2901545
Luciano_Amarante <lucianoa2028@gmail.com>
2021-06-30 16:55:45 -0300

    Menu
    1 - Trocado o icone Close

--- app/src/main/java/br/com/ptz/controle/sistema/ConectDBAcontrole.kt
+++ app/src/main/java/br/com/ptz/controle/sistema/ConectDBAcontrole.kt
@@ -1,11 +1,5 @@
 package br.com.ptz.controle.sistema
 
-import android.widget.*
-import android.widget.Toast
-import java.sql.DriverManager
-import java.util.*
-import java.sql.*
-import java.util.Properties
 
 class ConectDBAcontrole {
 
